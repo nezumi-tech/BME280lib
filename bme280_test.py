7ff0a92258d3d2da2c9f5d6346f1396813f95f1a
nezumi_tech <nezumi_tech@yahoo.co.jp>
2018-12-02 20:12:22 +0900

    add comment

--- bme280_test.py
+++ bme280_test.py
@@ -1,9 +1,14 @@
+#読み込み
 import bme280
 
+#初期設定
 bme280.setup()
 bme280.get_calib_param()
 
+#温度(℃),気圧(hPa),湿度(%)の順でタプルとして出てくるので、アンパックして変数に代入
 temp,pres,hum =bme280.readData()
+
+#各値を表示
 print(temp)
 print(pres)
 print(hum)
