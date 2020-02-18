04a2ed100e78886a2710639830b32c006439bfa5
nezumi_tech <nezumi_tech@yahoo.co.jp>
2020-02-19 04:22:01 +0900

    fix typo

--- bme280_test.py
+++ bme280_test.py
@@ -6,7 +6,7 @@ bme280.setup()
 bme280.get_calib_param()
 
 #温度(℃),気圧(hPa),湿度(%)の順でタプルとして出てくるので、アンパックして変数に代入
-temp,pres,hum =bme280.readData()
+temp,pres,hum = bme280.readData()
 
 #各値を表示
 print(temp)
